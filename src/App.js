b03d87b8b6d2950b74415278c6122539cea1648c
payalpahilani <58146637+payalpahilani@users.noreply.github.com>
2024-04-08 19:13:12 -0400

    todo-app assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  // State variables for todos, selected user, and loading state
+  const [todos, setTodos] = useState([]);
+  const [userId, setSelectedUser] = useState('1');
+  const [loading, setLoadingState] = useState(false);
+
+  // Fetch todos whenever the selected user changes
+  useEffect(() => {
+    setLoadingState(true); // Set loading state to true while fetching data
+
+    fetch(`https://dummyjson.com/todos/user/${userId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data.todos); // Update todos state with fetched data
+        setLoadingState(false); // Set loading state to false after data is loaded
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setLoadingState(false); // Set loading state to false if an error occurs
+      });
+  }, [userId]); // Dependency array ensures useEffect runs when userId changes
+
+  // Handler to update userId based on user selection
+  const handleChange = (event) => {
+    setSelectedUser(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section className="app-content">
+      <header>
+        <h1 className="heading">Todos</h1>
       </header>
-    </div>
+      <div>
+        <label htmlFor="user">Please select a user : </label>
+        <select id="user" onChange={handleChange} className="dropdown">
+          <option value="1">Arthur</option>
+          <option value="2">Lily</option>
+          <option value="3">George</option>
+        </select>
+      </div>
+      <main>
+        {loading ? (
+          <p>Data is getting loaded</p>
+        ) : (
+          <ul>
+            {todos.map((item) => (
+              <li key={item.id}>{item.todo}</li>
+            ))}
+          </ul>
+        )}
+      </main>
+    </section>
   );
-}
+};
 
 export default App;
